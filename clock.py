1781e12e09efaeeb4bb690615b1553ff7c32cab7
RobertPoncelet <robert.poncelet@talk21.com>
2019-10-12 23:25:31 +0000

    Clock digit transitions are *almost* working but there's something wrong with the step numbering

--- clock.py
+++ clock.py
@@ -1,26 +1,52 @@
-import animation
+from animation import Animation
+from PIL import Image, ImageChops
 import datetime
 
 class ClockAnimation(Animation):
     def __init__(self, buffer):
         super().__init__(buffer)
-        self.drawn = False
-        now = datetime.datetime.now().time()
-        self.time = str(now.hour) + str(now.minute)
+        self.oldtime = "0000"
+        self.time = "0001" # TODO: remove
+        self.refresh_time()
+        self.step = 0
         self.sheet = Image.open("sheet.png")
 
+    def refresh_time(self):
+        #now = datetime.datetime.now().time()
+        #hour = str(now.hour).zfill(2)
+        #minute = str(now.minute).zfill(2)
+        #self.time = hour + minute
+        pass
+
+    def sheet_box(self, digit, step, movein):
+        if not movein:
+            step += 4
+        return (digit*8, step*8, digit*8+8, step*8+8)
+
     def should_update(self):
-        return not self.drawn
+        self.refresh_time()
+        return self.time != self.oldtime
 
     def has_finished(self):
-        return self.drawn
+        return False
 
     def update(self):
         for d in enumerate(self.time):
             i = d[0]
+            if self.oldtime[i] == d[1]:
+                continue
             digit = int(d[1])
-            srcbox = [8*digit, 4*8, 8*digit+8, 4*8+8]
-            dstbox = [8*i, 0]
-            self.buffer.paste(self.sheet.crop(srcbox), dstbox)
-            #self.buffer.convert("1")
-        self.drawn = True
+            insrcbox = self.sheet_box(digit, self.step, True)
+            inim = self.sheet.crop(insrcbox).convert("L")
+            dstbox = (8*i, 0)
+            if self.step < 5:
+                outdigit = int(self.oldtime[i])
+                outsrcbox = list(self.sheet_box(outdigit, self.step, False))
+                outim = self.sheet.crop(outsrcbox).convert("L")
+                inim = ImageChops.add(inim, outim)
+            self.buffer.paste(inim, dstbox)
+        self.step += 1
+        if self.step >= 5:
+            self.oldtime = self.time
+            self.time = str(int(self.time)+1).zfill(4) # TODO: remove
+            self.step = 0
--- ledflinger.py
+++ ledflinger.py
@@ -26,8 +26,8 @@ layers = [Layer(size), Layer(size, flags=flaggy), Layer(size, flags=flaggy), Lay
 text_anim = TextAnimation(None, "Hello")
 first_anim = CircleAnimation(None, WHITE, (0, 0), (screen_width, screen_height), waiting_for=text_anim)
 layers[0].add_animation(first_anim)
-layers[0].add_animation(CircleAnimation(None, WHITE, (0, screen_height), (screen_width, 0), waiting_for=text_anim))
-layers[1].add_animation(CircleAnimation(None, WHITE, (screen_width, 0), (0, screen_height), waiting_for=first_anim))
+#layers[0].add_animation(CircleAnimation(None, WHITE, (0, screen_height), (screen_width, 0), waiting_for=text_anim))
+#layers[1].add_animation(CircleAnimation(None, WHITE, (screen_width, 0), (0, screen_height), waiting_for=first_anim))
 layers[2].add_animation(text_anim)
 #layers[3].add_animation(BackgroundAnimation(None, WHITE))
 comp.layers = layers
@@ -40,8 +40,6 @@ def test(comp):
 
     while ping.isPhoneAvailable():
         time.sleep(1.)
-        with comp.lock:
-            layers[0].add_animation(CircleAnimation(None, WHITE, (0, screen_height), (screen_width, 0)))
 
     with comp.lock:
         comp.done = True
