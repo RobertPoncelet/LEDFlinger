8695935023dc21de26c5351b74d9485b1756b0f7
RobertPoncelet <robert.poncelet@talk21.com>
2019-10-14 21:39:58 +0000

    Added that flashing colon that you see on most digital clocks

--- clock.py
+++ clock.py
@@ -1,5 +1,6 @@
 from animation import Animation
 from PIL import Image, ImageChops
+from colours import BLACK, WHITE
 import datetime
 
 class ClockAnimation(Animation):
@@ -8,12 +9,17 @@ class ClockAnimation(Animation):
         self.oldtime = "9999"
         self.refresh_time()
         self.step = 0
+        self.show_colon = False
+        self.old_show_colon = False
         self.sheet = Image.open("sheet.png")
+        # TODO: make sure animations are initialised with a proper buffer, then uncomment the next line
+        #self.buffer.paste(BLACK, (0, 0, buffer.width, buffer.height))
 
     def refresh_time(self):
         now = datetime.datetime.now().time()
-        hour = str(now.hour).zfill(2) # TODO: change back
-        minute = str(now.minute).zfill(2)
+        hour = str(now.minute).zfill(2) # TODO: change back
+        minute = str(now.second).zfill(2)
+        self.show_colon = now.microsecond < 500000
         self.time = hour + minute
 
     def sheet_box(self, digit, step, movein):
@@ -23,12 +29,18 @@ class ClockAnimation(Animation):
 
     def should_update(self):
         self.refresh_time()
-        return self.time != self.oldtime
+        return self.time != self.oldtime or self.show_colon != self.old_show_colon
 
     def has_finished(self):
         return False
 
     def update(self):
+        if self.time != self.oldtime:
+            self.update_digits()
+        if self.show_colon != self.old_show_colon:
+            self.update_colon()
+
+    def update_digits(self):
         for d in enumerate(self.time):
             i = d[0]
             if self.oldtime[i] == d[1]:
@@ -36,7 +48,9 @@ class ClockAnimation(Animation):
             digit = int(d[1])
             insrcbox = self.sheet_box(digit, self.step, True)
             inim = self.sheet.crop(insrcbox).convert("L")
-            dstbox = (8*i, 0)
+            dstbox = [8*i, 0]
+            if i >= 2:
+                dstbox[0] += 1
             if self.step < 5:
                 outdigit = int(self.oldtime[i])
                 outsrcbox = list(self.sheet_box(outdigit, self.step, False))
@@ -47,3 +61,9 @@ class ClockAnimation(Animation):
         if self.step >= 5:
             self.oldtime = self.time
             self.step = 0
+
+    def update_colon(self):
+        colour = WHITE if self.show_colon else BLACK
+        self.buffer.putpixel((16, 2), colour)
+        self.buffer.putpixel((16, 5), colour)
+        self.old_show_colon = self.show_colon
